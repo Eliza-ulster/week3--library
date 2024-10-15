6e9dd5786af33809e2998b8d9c1b8af33a107039
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-15 12:05:43 +0100

    week4

---
+++ LiberaryTests/MemberTests.cs
@@ -0,0 +1,16 @@
+using Library;
+
+namespace LiberaryTests
+{
+    [TestClass]
+    public class MemberTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+            // Arrange 
+
+            // Act 
+        {
+        }
+    }
+}
